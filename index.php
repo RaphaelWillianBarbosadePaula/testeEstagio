a2c5ae77358e04740ff4726fbb522d0308475590
Raphael Willian Barbosa de Paula <120695571+RaphaelWillianBarbosadePaula@users.noreply.github.com>
2023-03-11 10:34:15 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+include("views/header.php");
+include("views/footer.php");
+
+?>
+
+<div class="barra">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-md">
+            <a class="navbar-brand" href="#">
+            <img src="images/tatu.png" alt="" width="40" height="40" class="d-inline-block align-text-top">
+            <b> - SEJA BEM-VINDO! - </b>
+            <img src="images/tatu.png" alt="" width="40" height="40" class="d-inline-block align-text-top">
+            </a>
+        </div>
+    </nav>
+</div>
+
+<br>
+
+<div class="container">
+	<h1><strong>Você está na tela inicial!</strong></h1>
+	<p>
+		Clique no botão para ir à tela de usuários.
+	</p>
+    <br>
+    <br>
+    <div class="botaoInicial">
+        <b><a href="views/usuario.php"> Usuários </a></b>
+    </div>
+</div>
+
+<br><br>
+    <div class="copy">
+        <p> © Copyright Raphael Willian Barbosa de Paula </p>
+    </div>
---
+++ testeEstagio.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.5.5-10.4.8-MariaDB : Database - testeestagio
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`testeestagio` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `testeestagio`;
+
+/*Table structure for table `usuarios` */
+
+DROP TABLE IF EXISTS `usuarios`;
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `senha` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `usuarios` */
+
+insert  into `usuarios`(`id`,`nome`,`email`,`senha`) values (1,'Raphael','raphaelwillian.bdp@gmail.com','RAPHAAA'),(2,'Thalyson','thalyson@gmail.com','Thalyson1234'),(3,'Willian','will@gmail.com','1234'),(4,'Gabriel','gabriel@gmail.com','Gab777');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
